1b897567303da0e76b1655ccdb06d92c95cb6211
Hampus L. (OSX) <hampuslidin@gmail.com>
2015-01-27 12:54:09 +0100

    Changed FA.vhd to fa.vhd, and modified the file with the updated name

--- FA.vhd
+++ FA.vhd
@@ -1,16 +1,13 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-ENTITY FA IS 
+ENTITY fa IS 
 	PORT( a, b, cin : in STD_LOGIC;
 	      s, cout   : out STD_LOGIC);
-END FA;
+END fa;
 
-ARCHITECTURE dataflow OF FA IS
+ARCHITECTURE dataflow OF fa IS
 BEGIN
 	s <= a XOR b XOR cin;
 	cout <= (a AND b) OR (a AND cin) OR (b AND cin);
 END dataflow;
-
-
-	
