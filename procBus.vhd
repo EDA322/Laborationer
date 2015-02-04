f1f4aa9ce3538b34e3c127008bc5ce557a1629a1
Hampus L. (OSX) <hampuslidin@gmail.com>
2015-02-04 18:09:43 +0100

    Optimization for the bus mux

--- procBus.vhd
+++ procBus.vhd
@@ -2,7 +2,18 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
 
-ENTITY procBus IS    PORT ( INSTRUCTION : IN  std_logic_vector (7 DOWNTO 0);           DATA	       : IN  std_logic_vector (7 DOWNTO 0);           ACC 	       : IN  std_logic_vector (7 DOWNTO 0);           EXTDATA     : IN  std_logic_vector (7 DOWNTO 0);           OUTPUT      : OUT std_logic_vector (7 DOWNTO 0);           ERR         : OUT std_logic;           instrSEL    : IN  std_logic;           dataSEL     : IN  std_logic;           accSEL      : IN  std_logic;           extdataSEL  : IN  std_logic);END procBus;
+ENTITY procBus IS
+    PORT ( INSTRUCTION : IN  std_logic_vector (7 DOWNTO 0);
+           DATA	       : IN  std_logic_vector (7 DOWNTO 0);
+           ACC 	       : IN  std_logic_vector (7 DOWNTO 0);
+           EXTDATA     : IN  std_logic_vector (7 DOWNTO 0);
+           OUTPUT      : OUT std_logic_vector (7 DOWNTO 0);
+           ERR         : OUT std_logic;
+           instrSEL    : IN  std_logic;
+           dataSEL     : IN  std_logic;
+           accSEL      : IN  std_logic;
+           extdataSEL  : IN  std_logic);
+END procBus;
 
 ARCHITECTURE structure Of procBus IS
 SIGNAL enc_sel: STD_LOGIC_VECTOR(1 downto 0);
@@ -17,15 +28,19 @@ COMPONENT mux4to1
 	       sel : in STD_LOGIC_VECTOR(1 downto 0));
 END COMPONENT;
 BEGIN
-	ERR <= not ((not instrSEL and not dataSEL and not accSEL and not extdataSEL) or
-		    (    instrSEL and not dataSEL and not accSEL and not extdataSEL) or
-	            (not instrSEL and     dataSEL and not accSEL and not extdataSEL) or
-	            (not instrSEL and not dataSEL and     accSEL and not extdataSEL) or
-	            (not instrSEL and not dataSEL and not accSEL and     extdataSEL));
+-- Encoding for the bus mux selector.
+-- s1 = a nor b
+-- s0 = a nor c
+	enc_sel <= (instrSEL nor dataSEL) & (instrSEL nor accSEL);
+-- f = (a+c+d)·(a+b+c)·(b+c+d)·(a+b+d). This expression is valid given
+-- that when no select signal is set, the mux selection is don't care.
+	ERR <= (instrSEL or dataSEL or accSEL)     and
+		   (instrSEL or dataSEL or extdataSEL) and
+		   (instrSEL or accSEL  or extdataSEL) and
+		   (dataSEL  or accSEL  or extdataSEL);
 	mux: mux4to1
 		generic map(width => 8)
 		port map (w1 => INSTRUCTION, w2 => DATA, w3 => ACC, w4 => EXTDATA,
 				f => OUTPUT, sel => enc_sel);
-	enc_sel <= (instrSEL NOR dataSEL) & (instrSEL NOR accSEL);
 END structure;
 
